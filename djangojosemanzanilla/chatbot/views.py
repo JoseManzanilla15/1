31ad2d48ee5671ed283deb7272bc6afc3ab775d9
JoseManzanilla15 <manzber15@gmail.com>
2026-04-17 18:12:05 -0400

    Add files via upload

---
+++ djangojosemanzanilla/chatbot/views.py
@@ -0,0 +1,85 @@
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework.decorators import action
+from django.contrib.auth.models import User
+from rest_framework_simplejwt.authentication import JWTAuthentication
+from chatbot.models import Conversacion
+from chatbot.serializers import ConversacionSerializer
+from chatbot.rag import generar_respuesta
+from inventario.models import Producto
+
+
+class ChatbotView(APIView):
+    serializer_class = ConversacionSerializer
+    authentication_classes = [JWTAuthentication]
+
+    def post(self, request):
+        serializer = self.serializer_class(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        engine = request.query_params.get('engine', "groq").lower()
+        pregunta = serializer.validated_data["pregunta"].lower()
+
+        productos = Producto.objects.all()
+        contexto = "\n".join(
+            [f"Producto: {p.nombre.lower()} | Descripcion: {p.descripcion.lower()} | Precio: ${p.precio} | Cantidad: {p.cantidad} | Categoria: {p.categoria.nombre.lower()}" for p in productos])
+        
+        if not contexto:
+            return Response({"error":"no hay contexto"})
+            
+        SYSTEM_PROMPT = (
+            "Eres un ayudande en un tienda de dulces virtual. "
+            "Usa solo la información provista en el 'CONTEXT' para responder "
+            "consultas sobre productos, disponibilidad del stock y promociones. "
+            "Si no puedes responder con la información disponible, indica que no puedes responder.",
+            "Si te preguntan sobre la informacion de la que dispones, dile que hay en el stock y menciona las promociones"
+        )
+        
+        if request.user.is_authenticated:
+            historial_usuario = Conversacion.objects.filter(usuario=request.user).order_by('-fecha_creacion')[:3]
+            historial_plano = "".join([
+                f"\nPregunta: {h.pregunta.lower()}\nUsuario: {h.respuesta.lower()}" for h in historial_usuario
+            ])
+        else:
+            historial_plano = "Sin historial previo (usuario no autenticado).\n"
+        
+        prompt = (
+            f"{SYSTEM_PROMPT}\n\n"
+            f"=== CONTEXT ===\n{contexto}\n\n"
+            f"=== HISTORIAL RELEVANTE ===\n"
+            f"{historial_plano}\n"
+            f"\n=== INSTRUCCIÓN ===\n"
+            "\nResponde solo con información del contexto. Sé claro, conciso y emite la respuesta en texto plano.\n"
+        )
+        
+        temperatura = serializer.validated_data.get("temperatura", 0.7)
+        modelo = serializer.validated_data.get("modelo", "modelo_base")
+        
+        try:
+            print("Prompt:", prompt)
+            print("Haciendo petición con el modelo:", modelo)
+            respuesta = generar_respuesta(
+                prompt, pregunta, temperatura=temperatura, engine=engine)
+            print("Respuesta:", respuesta)
+            
+            LISTA_ERROR = ["No puedo responder a esa pregunta.",
+                           "No puedo responder a tu pregunta.", "No puedo responder a tu pregunta", ""]
+            
+            if respuesta and respuesta not in LISTA_ERROR:
+                print("Guardando conversacion")
+                if request.user.is_authenticated:
+                    Conversacion.objects.create(
+                        pregunta=pregunta, 
+                        respuesta=respuesta, 
+                        usuario=request.user, 
+                        temperatura=temperatura
+                    )
+                else:
+                    print("Usuario anónimo - no se guarda conversación")
+                
+                return Response({"pregunta": pregunta, "respuesta": respuesta})
+                
+        except Exception as e:
+            return Response({"error": str(e)}, status=500)
+
+        return Response({"pregunta": pregunta, "respuesta": respuesta})
---
+++ djangojosemanzanilla/djangojosemanzanilla/urls.py
@@ -0,0 +1,25 @@
+from django.contrib import admin
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+from inventario.views import InventarioListView, InventarioDetailView, CategoriaListView, CategoriaDetailView
+from cuentas.views import UsuarioViewSet
+from tienda.views import CompraViewSet, VentaViewSet
+from chatbot.views import ChatbotView
+
+router = DefaultRouter()
+router.register('usuario', UsuarioViewSet, basename='usuarios')
+router.register('compra', CompraViewSet, basename='compras')
+router.register('venta', VentaViewSet, basename='ventas')
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('api/login/', TokenObtainPairView.as_view(), name='login'),
+    path('api/refresh/', TokenRefreshView.as_view(), name='refresh token'),
+    path('api/categoria/', CategoriaListView.as_view(), name="categoria"),
+    path('api/categoria/<int:pk>/', CategoriaDetailView.as_view(), name="categoria-detalle"),
+    path('api/inventario/', InventarioListView.as_view(), name="inventario"),
+    path('api/inventario/<int:pk>/', InventarioDetailView.as_view(), name="inventario-detalle"),
+    path('api/chatbot/', ChatbotView.as_view(), name="chatbot"),
+    path('api/', include(router.urls)),
+]
